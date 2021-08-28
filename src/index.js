71cc9546d41478fd00d9d8507232a450d55ac8a6
Francisco García <francoffee57@gmail.com>
2021-08-28 16:56:04 -0500

    Agregué el botón de play y lo hice funcionar, tambien agregue el gitignore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hey! Todo bien :D')
+const video = document.getElementById('video');
+const playButton = document.getElementById('media-play');
+
+class MediaPlayer{
+ constructor(config){
+     this.media = config.el;
+ }
+ playMedia(){
+     this.media.play();
+ }
+ pauseMedia(){
+    this.media.pause();
+ }
+}
+
+const play = new MediaPlayer({ el : video });
+
+playButton.onclick = () => {
+    // Comprobamos si el video esta pausado
+    if(video.paused){
+         play.playMedia()
+    } 
+    else{
+         play.pauseMedia()
+    }
+}
